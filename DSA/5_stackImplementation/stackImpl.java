d4149abfb1dea6c0662be14132b626c4317c389c
sidxyz0 <sidxyz0@gmail.com>
2022-11-20 16:28:27 +0530

    Fixed stack

--- DSA/5_stackImplementation/stackImpl.java
+++ DSA/5_stackImplementation/stackImpl.java
@@ -23,18 +23,16 @@ class Stack<T> {
     public T pick() {
         T val;
         if (this.stackArr.size() > 1) {
-            this.stackArr.remove(this.stackArr.size()-1);
-            this.top = this.stackArr.get(this.stackArr.size()-1);
-            this.length = this.stackArr.size();
             val = this.top;
-        }
-        else if(this.stackArr.size() == 1){
-            this.stackArr.remove(this.stackArr.size()-1);
+            this.stackArr.remove(this.stackArr.size() - 1);
+            this.top = this.stackArr.get(this.stackArr.size() - 1);
+            this.length = this.stackArr.size();
+        } else if (this.stackArr.size() == 1) {
+            val = this.top;
+            this.stackArr.remove(this.stackArr.size() - 1);
             this.top = null;
             this.length = this.stackArr.size();
-            val = this.top;
-        }
-        else{
+        } else {
             System.out.println("Stack is empty. ");
             val = null;
         }
@@ -44,9 +42,8 @@ class Stack<T> {
 
     public T peek() {
         if (this.stackArr.size() > 0) {
-            return this.stackArr.get(this.stackArr.size() - 1);
-        }
-        else{
+            return this.top;
+        } else {
             System.out.println("Stack is empty. ");
             return null;
         }
@@ -55,23 +52,111 @@ class Stack<T> {
     public int getSize() {
         return this.length;
     }
+
+    public void putAt(int n, T val) {
+        Stack<T> b = new Stack<T>();
+        while (this.length > n) {
+            b.put(this.pick());
+        }
+        this.put(val);
+        while (b.length > 0) {
+            this.put(b.pick());
+        }
+    }
+
+    public T pickAt(int n) {
+        Stack<T> b = new Stack<T>();
+        while (this.length >= n) {
+            b.put(this.pick());
+        }
+        T a = b.pick();
+        while (b.length >= 0) {
+            this.put(b.pick());
+        }
+
+        return a;
+    }
+
+    public T peekAt(int n) {
+        Stack<T> b = new Stack<T>();
+        while (this.length >= n) {
+            b.put(this.pick());
+        }
+        T a = b.peek();
+        while (b.length >= 0) {
+            this.put(b.pick());
+        }
+
+        return a;
+    }
+
+    public void reverseIt() {
+        Stack<T> b = new Stack<T>();
+        Stack<T> c = new Stack<T>();
+        while (this.length > 0) {
+            b.put(this.pick());
+        }
+        while (b.length > 0) {
+            c.put(b.pick());
+        }
+        while (c.length > 0) {
+            this.put(c.pick());
+        }
+    }
+
+    public Stack<T> getReverse() {
+        Stack<T> b = new Stack<T>();
+        while (this.length > 0) {
+            b.put(this.pick());
+        }
+        return b;
+    }
+
+    public String stackToString() {
+        int l = this.length;
+        Stack<T> c = new Stack<T>();
+        Stack<T> b = new Stack<T>();
+        StringBuilder s = new StringBuilder("");
+        if (l > 1) {
+            while (this.length > 0) {
+                if (this.length == 1) {
+                    s.insert(0,"["+this.peek());
+                    // s.append("["+this.peek());
+                    b.put(this.pick());
+                } else {
+                    s.insert(0,", " + this.peek());
+                    // s.append(this.peek() + ", ");
+                    b.put(this.pick());
+                }
+            }
+            while (b.length > 0) {
+                this.put(b.pick());
+            }
+            s.append("]");
+        } else if (l == 1) {
+            s.append("[" + this.peek().toString() + "]");
+        } else {
+            System.out.println("Stack is empty. ");
+        }
+        return s.toString();
+    }
 }
 
 public class stackImpl {
     public static void main(String args[]) {
         Stack<Integer> a = new Stack<Integer>();
 
-        //Test cases
+        // Test cases
         a.put(1);
         a.put(2);
         a.put(3);
         a.put(4);
         System.out.println(a.peek());
         System.out.println(a.pick());
-        System.out.println(a.pick());
-        System.out.println(a.pick());
-        System.out.println(a.pick());
-        System.out.println(a.pick());
-        System.out.println(a.peek());
+        a.put(5);
+        a.reverseIt();
+        a.putAt(1, 6);
+        System.out.println(a.stackToString());
+
     }
 }
